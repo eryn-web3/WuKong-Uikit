658f406eb0f1b2888dcb4bcdeacb30c516eab7f5
Sun Dong <57502391+sundong-exideatech@users.noreply.github.com>
2022-08-27 18:24:36 +0800

    initial deployment

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# WuKong-Uikit
+# 🥞 Wukong UIkit
+
+[![Version](https://img.shields.io/npm/v/@wukongfinance/uikit)](https://www.npmjs.com/package/@wukongfinance/uikit) [![Size](https://img.shields.io/bundlephobia/min/@wukongfinance/uikit)](https://www.npmjs.com/package/@wukongfinance/uikit)
+
+Wukong UIkit is a set of React components and hooks used to build pages on Wukong's apps. It also contains a theme file for dark and light mode.
+
+## Install
+
+`yarn add @wukongfinance/uikit`
+
+## Setup
+
+### Theme
+
+Before using Wukong UIkit, you need to provide the theme file to styled-component.
+
+```
+import { ThemeProvider } from 'styled-components'
+import { light, dark } from '@wukongfinance/uikit'
+...
+<ThemeProvider theme={isDark}>...</ThemeProvider>
+```
+
+### Reset
+
+A reset CSS is available as a global styled component.
+
+```
+import { ResetCSS } from '@wukongfinance/uikit'
+...
+<ResetCSS />
+```
---
+++ src/widgets/Menu/config.ts
@@ -0,0 +1,164 @@
+export const links = [
+  {
+    label: "Home",
+    icon: "HomeIcon",
+    href: "/",
+  },
+  {
+    label: "Trade",
+    icon: "TradeIcon",
+    items: [
+      {
+        label: "Exchange",
+        href: "https://exchange.pancakeswap.finance",
+      },
+      {
+        label: "Liquidity",
+        href: "https://exchange.pancakeswap.finance/#/pool",
+      },
+    ],
+  },
+  {
+    label: "Farms",
+    icon: "FarmIcon",
+    href: "/farms",
+  },
+  {
+    label: "Pools",
+    icon: "PoolIcon",
+    href: "/syrup",
+  },
+  {
+    label: "Launchpad",
+    icon: "BushIcon",
+    href: "/bush",
+  },
+  {
+    label: 'IFO',
+    icon: 'IfoIcon',
+    href: 'https://wukong.finance/#/ifo',
+  },
+  {
+    label: 'Audit',
+    icon: 'ShieldIcon',
+    href: 'https://wukong.finance/files/techaudit_wukong.pdf',
+  },
+  {
+    label: 'Listing',
+    icon: 'InfoIcon',
+    items: [
+      {
+        label: 'EtherScan',
+        href: '#',
+      },
+      {
+        label: 'DappRadar',
+        href: '#',
+      },
+      {
+        label: 'CoinGecko',
+        href: '#',
+      },
+      {
+        label: 'CoinMarketCap',
+        href: '#',
+      },
+    ]
+  },
+  {
+    label: 'Analytics',
+    icon: 'ListViewIcon',
+    items: [
+      {
+        label: 'Overview',
+        href: 'https://info.wukong.finance',
+        target: '_blank',
+      },
+      {
+        label: 'Tokens',
+        href: 'https://info.wukong.finance/tokens',
+        target: '_blank',
+      },
+      {
+        label: 'Pairs',
+        href: 'https://info.wukong.finance/pairs',
+        target: '_blank',
+      },
+      {
+        label: 'Accounts',
+        href: 'https://info.wukong.finance/accounts',
+        target: '_blank',
+      },      
+    ],
+  },
+  {
+    label: 'More',
+    icon: 'MoreIcon',
+    items: [
+      {
+        label: 'Github',
+        href: 'https://github.com/WuKong-Finance/',
+        target: '_blank',
+      },
+      {
+        label: 'Docs',
+        href: "https://WuKong.gitbook.io/wukongfinance/",
+        target: '_blank',
+      },
+      {
+        label: 'NFT Marketplace',
+        href: '#',
+        target: '_blank',
+      },
+      {
+        label: 'Blog',
+        href: 'https://wukongfinance.medium.com/',
+        target: '_blank',
+      },
+      {
+        label: 'Partnership',
+        href: '#',
+        target: '_blank',
+      },
+    ],
+  },
+  // {
+  //   label: 'Referrals',
+  //   icon: 'GroupsIcon',
+  //   href: 'https://wukong.finance/#/referrals',
+  // },
+  // {
+  //   label: 'Lottery',
+  //   icon: 'TicketIcon',
+  //   href: 'https://wukong.finance/#/lottery',
+  // },
+  // {
+  // {
+  //   label: 'NFT Marketplace',
+  //   icon: 'NftIcon',
+  //   href: 'https://nft.wukong.finance',
+  // },
+  // {
+  //   label: 'NFT Gaming',
+  //   icon: 'MoonIcon',
+  //   href: 'https://wukong.finance/#/nftgaming',
+  // },
+];
+
+export const socials = [
+  {
+    label: "Telegram",
+    icon: "TelegramIcon",
+    href: "https://t.me/WukongEth",
+  },
+  {
+    label: "Twitter",
+    icon: "TwitterIcon",
+    href: "https://twitter.com/Wukong_Eth",
+  },
+];
+
+export const MENU_HEIGHT = 64;
+export const MENU_ENTRY_HEIGHT = 48;
+export const SIDEBAR_WIDTH_FULL = 240;
+export const SIDEBAR_WIDTH_REDUCED = 56;
